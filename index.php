b9de80f8dd6bedf85333764dfcbb64a5bce40b4a
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2019-03-28 09:23:16 -0500

    initial structure

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The Custom Report setup page.
+ *
+ * @package   customreportuv
+ * @copyright  2019 Camilo José Cruz Rivera <cruz.camilo@correounivalle.edu.co>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once '../../config.php';
+require_once 'manager/report_lib.php';
+require_once($CFG->libdir.'/adminlib.php');
+
+$courseid = required_param('id', PARAM_INT);
+$url = new moodle_url('/local/customgrader/index.php', array('id' => $courseid));
+
+// $PAGE->set_url($url);
+$PAGE->set_title("Reporte Univalle");
+
+// $PAGE->set_pagelayout('admin');
+$extraurlparams = array('id' => $courseid);
+
+admin_externalpage_setup('customreportuv','', $extraurlparams, "$CFG->wwwroot/local/customreportuv/index.php");
+echo $OUTPUT->header();
+
+echo $OUTPUT->heading("REPORTE");
+
+// echo $OUTPUT->container(get_string('err_nousers', 'completion'), 'errorbox errorboxcontent');
+echo $OUTPUT->footer();
+
+// echo get_info_course($courseid);
---
+++ manager/report_lib.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Grader Lib
+ *
+ * @author     Camilo José Cruz Rivera
+ * @package    customreportuv
+ * @copyright  2019 Camilo José Cruz Rivera <cruz.camilo@correounivalle.edu.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Queries from module custom report UV
+
+/**
+ * Gets course information given its id
+ * @see get_info_course($id_curso)
+ * @param $id_curso --> course id
+ * @return array Containing all calificable test report
+ */
+
+function get_info_course($id_curso)
+{
+    return "HOLA";
+}
